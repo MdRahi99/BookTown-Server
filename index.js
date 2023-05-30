15acd68c9024e1ddc6fb67b249e7e48b7a01032d
MD Rahi <fhrstation21@gmail.com>
2023-05-30 21:11:29 +0600

    specific category api created

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nxhpsct.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const booksCategory = client.db('BookTown').collection('BooksCategory');
+
+    app.get('/books-category', async(req, res) => {
+        const query = {};
+        const cursor = booksCategory.find(query);
+        const category = await cursor.toArray();
+        res.send(category);
+    });
+
+    app.get('/books-category/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const categoryDetails = await booksCategory.findOne(query);
+      res.send(categoryDetails);
+    });
+  } 
+  finally {}
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('BookTown Server Running...');
