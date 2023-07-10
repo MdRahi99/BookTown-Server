e9f2e65dbc230fcc70032bd89cdbfd1d39ed0280
MD Rahi <fhrstation21@gmail.com>
2023-07-10 21:37:45 +0600

    ...

--- index.js
+++ index.js
@@ -21,30 +21,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const booksCategory = client.db('BookTown').collection('BooksCategory');
+    const booksDetails = client.db('BookTown').collection('BooksDetails');
 
-    app.get('/books-category', async(req, res) => {
-        const query = {};
-        const cursor = booksCategory.find(query);
-        const category = await cursor.toArray();
-        res.send(category);
+    app.get('/books-category', async (req, res) => {
+      const query = {};
+      const cursor = booksCategory.find(query);
+      const category = await cursor.toArray();
+      res.send(category);
     });
-
-    app.get('/books-category/:id', async(req, res) => {
+    app.get('/books-category/:id', async (req, res) => {
       const id = req.params.id;
       const query = { '_id': new ObjectId(id) };
       const categoryDetails = await booksCategory.findOne(query);
       res.send(categoryDetails);
     });
-  } 
-  finally {}
+    app.get('/books-details', async (req, res) => {
+      const query = {};
+      const cursor = booksDetails.find(query);
+      const books = await cursor.toArray();
+      res.send(books);
+    });
+    app.get('/books-details/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { '_id': new ObjectId(id) };
+      const bookDetails = await booksDetails.findOne(query);
+      res.send(bookDetails);
+    });
+
+  }
+  finally { }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('BookTown Server Running...');
+  res.send('BookTown Server Running...');
 });
 
 app.listen(port, () => {
-    console.log(`BookTown Server Running On Port: ${port}`);
+  console.log(`BookTown Server Running On Port: ${port}`);
 });
