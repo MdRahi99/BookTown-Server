70ec7c3aa1ac03c9fb2ad03e6fb6d0cd842cda8d
MD Rahi <fhrstation21@gmail.com>
2023-11-06 20:37:11 +0600

    ...

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function run() {
     const booksDetails = client.db('BookTown').collection('BooksDetails');
     const userAddedBooks = client.db('BookTown').collection('AddedBooks');
     const cartCollection = client.db('BookTown').collection('CartCollection');
+    const paymentCollection = client.db("BookTown").collection("Payments");
     const contactDetails = client.db('BookTown').collection('ContactDetails');
 
     // /////////////// JWT ////////////////
@@ -264,7 +265,7 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+    app.post('/create-payment-intent', async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
@@ -276,6 +277,15 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret
       })
+    });
+
+    app.post('/payments', verifyJWT, async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+
+      const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+      const deleteResult = await cartCollection.deleteMany(query)
+      res.send({ insertResult, deleteResult });
     })
     // ..............User Cart............ //
 
