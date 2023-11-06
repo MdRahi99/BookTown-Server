8319b9c090a7003fdb0943f28d5f6a2360eac643
MD Rahi <fhrstation21@gmail.com>
2023-11-06 18:46:45 +0600

    create payment intent post api

--- index.js
+++ index.js
@@ -266,7 +266,7 @@ async function run() {
 
     app.post("/create-payment-intent", verifyJWT, async(req, res) => {
       const {price} = req.body;
-      const amount = price * 100;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: 'usd',
